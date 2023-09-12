9c315f05a74ecdf788e8b54f6bebd182d9d88f54
tracybrtn <tracynburton@gmail.com>
2023-09-12 19:01:04 +0200

    created basic project structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+//import React
+import React from "react";
+
+//import components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <main>
+        <h2>This is a test</h2>
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+//import React
+import React from "react"
+
+function Footer() {
+  return(
+    <footer>
+      Copyright 2023
+    </footer>
+  )
+}
+
+//export footer
+export default Footer;
